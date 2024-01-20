488997eb92bf5d31c4c6b8bb8227dc7a88a8ab56
xuananh2212 <xuantuananh2212@gmail.com>
2024-01-21 01:39:16 +0700

    update code

--- day56/auth/app.js
+++ day56/auth/app.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 
 passport.use(googlePassport);
 passport.serializeUser(function (user, done) {
